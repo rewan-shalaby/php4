fdde9d2f1b218eef82ac7b5fb4605e62e5214486
rewan <rewan.shalaby5@gmail.com>
2022-12-11 22:35:46 +0200

    first commit

---
+++ demo.php
@@ -0,0 +1,40 @@
+<?php
+
+$students = [
+    ['name' => 'Alaa', 'email' => 'ahmed@test.com', 'status' => 'Science'],
+    ['name' => 'Shamy', 'email' => 'ali@test.com', 'status' => 'AAST'],
+    ['name' => 'Youssef', 'email' => 'basem@test.com', 'status' => 'AAST'],
+    ['name' => 'Waleid', 'email' => 'farouk@test.com', 'status' => 'Science'],
+    ['name' => 'Rahma', 'email' => 'hany@test.com', 'status' => 'AAST'],
+];
+echo "<table>";
+echo "<th>Name</th>","<th>Email</th>","<th>Status</th>";
+foreach($students as $key) { 
+    if($key['status']=='Science'){
+        echo "<tr style = 'color:red;'>";
+    }else{
+        echo "<tr>";
+    }
+        echo "<td>".$key['name'] ."<br>"."</td>";
+        echo "<td >".$key['email'] ."<br>"."</td>";
+        echo "<td >".$key['status'] ."<br>"."</td>";
+        echo "</tr>";
+          
+}
+
+
+
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Application Name: PHP Class Registration</h1>
+</body>
+</html>
---
+++ part1.php
@@ -0,0 +1,158 @@
+<?php
+$nameErr = $emailErr = $genderErr = $detailsErr =$coursesErr =$agreeErr = "";
+$name = $email = $gender = $details =$courses =$agree = "";
+// if (isset ($_POST['submit'])) 
+ if ($_SERVER['REQUEST_METHOD']=="POST")
+	{
+	if (empty($_POST["name"])) {
+		$nameErr = "Name is required";
+	  }
+	   else {
+		$name = $_POST["name"];
+	// if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+	// 	  $nameErr = "Only letters and white space allowed";}
+		}
+	  }
+	if (empty($_POST["email"])) {
+		$emailErr = "Email is required";
+	  } else {
+		$email = $_POST["email"];
+	// if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	// 	  $emailErr = "Invalid email format";
+	//   }
+	  }
+	if (!empty($_POST["group"])) {
+		$group = $_POST["group"];
+	  } 
+	if (!empty($_POST["details"])) {
+		$details = $_POST["details"];
+	  }
+ 
+
+	if (empty($_POST["gender"])) {
+		$genderErr = "Gender is required";
+	  } else {
+		$gender = $_POST["gender"];
+	  }
+	  if (empty($_POST["agree"])) {
+		$agreeErr = "you must agree to terms";
+	  } else {
+		$agree = $_POST["agree"];
+	  }
+	// }
+
+	//   function test_input($data) {
+	// 	// $data = trim($data);
+	// 	// $data = stripslashes($data);
+	// 	// $data = htmlspecialchars($data);
+	// 	return $data;
+	//   }
+		// if(!empty($_GET["name"]) && !empty($_GET["email"]) && !empty($_GET["agree"]) && !empty($_GET["gender"])) {
+		// 	echo "<h2>Your given values are as:</h2>";
+		// 	echo "Name:".$_GET["name"];
+		// 	echo "<br>";
+		// 	echo "E-mail:".$_GET["email"];
+		// 	echo "<br>";
+		// 	echo "Group:".$_GET["group"];
+		// 	echo "<br>";
+		// 	echo "Class:".$_GET["details"];
+		// 	echo "<br>";
+		// 	echo "Gender:".$_GET["gender"];
+		// 	echo "<br>";
+		// 	echo "Your courses are:".$_GET["courses"];
+		// }
+		?>
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+	<style>
+		span{
+			color: red;
+		}
+	</style>
+</head>
+<body>
+<h1>Application Name: AAST_BIS Class Registration</h1> 
+<span>* Required Field</span>
+		<form action="<?php $_PHP_SELF ?>" method="post">
+            <table>
+            <tr>
+            <td><label>Name:</label></td>
+			<td><input type="text" name="name">
+			<span class="error">* <?php echo $nameErr;?></span></td>
+			</tr>
+            <tr>
+            <td> <label>E-mail:</label></td>
+			<td><input type="text" name="email">
+			<span class="error">* <?php echo $emailErr;?></span></td>
+			</tr>
+            <tr>
+            <td><label>	Group #:</label></td>
+			<td><input type="number" name="group"></td>
+			</tr>
+            <tr>
+            <td><label>	Class details:</label></td>
+			<td><textarea name="details" rows="5" cols="40"></textarea></td>
+			</tr>
+            <tr>
+            <td><label>	Gender:</label></td>
+			<td><input type="radio" name="gender" value="female">Female
+			<input type="radio" name="gender" value="male">Male
+			<span class="error">* <?php echo $genderErr;?></span></td>
+			</tr>
+            <tr>
+            <td><label>	Select Courses:</label></td>
+            <td>
+                <select name='courses[]' multiple>
+                <option>PHP</option>
+                <option>JavaScript</option>
+                <option>MySQL</option>
+                <option>HTML</option>
+                </select>
+            </td>
+            </tr>
+            <tr>
+            <td><label>Agree:</label></td>
+			<td><input type="checkbox" name="agree">
+			<span class="error">* <?php echo $agreeErr;?></span></td>
+			</tr>
+			<tr><td><input type="submit" name="submit"></td></tr>
+                </table>
+		</form>
+		<?php
+			echo "<h2>Your given values are as:</h2>";
+			echo "Name:".$name;
+			echo "<br>";
+			echo "E-mail:".$email;
+			echo "<br>";
+			echo "Group".$group;
+			echo "<br>";
+			echo "Class:".$details;
+			echo "<br>";
+			echo "Gender:".$gender;
+			echo "<br>";
+			if (!empty($_POST["courses"])) {
+				foreach($_POST["courses"] as $courses){
+				echo "Your courses are:".$courses;
+		}}
+			// echo "<h2>Your given values are as:</h2>";
+			// echo $name;
+			// echo "<br>";
+			// echo $email;
+			// echo "<br>";
+			// echo $group;
+			// echo "<br>";
+			// echo $details;
+            // echo "<br>";
+			// echo $gender;
+			// echo "<br>";
+			// echo $courses;
+		?>
+</body>
+
+</html>
